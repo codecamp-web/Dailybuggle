df70b8a2cd2b786177dfb91efb46071388e4eb2b
Edward Ogheneochuko <67348397+codecamp-web@users.noreply.github.com>
2025-04-23 03:19:43 +0100

    Add files via upload

--- src/app/components/MobileMenu.tsx
+++ src/app/components/MobileMenu.tsx
@@ -1,29 +1,66 @@
-"use client"
+import {
+    Drawer,
+    DrawerTrigger,
+    DrawerContent,
+    DrawerHeader,
+    DrawerFooter,
+    DrawerClose,
+  } from "@/components/ui/drawer"
+  import { Button } from "@/components/ui/button"
+  import { MenuIcon, ShieldCloseIcon } from "lucide-react"
+  import { useContext, useState } from "react"
+
+  import { Switch } from "@/components/ui/switch"
+import { ThemeContext } from "@/context/themeContext"
 
 
-import React from 'react'
-import { MenuIcon, ShieldCloseIcon } from "lucide-react"
-import { Switch } from "@/components/ui/switch"
-import { Button } from "@/components/ui/button"
-
-import { useState } from "react"
-
-
-const MobileMenu = () => {
+  const MobileNav = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+  
     const toggleMobileMenu = () => {
-        setIsMobileMenuOpen(!isMobileMenuOpen)
+      setIsMobileMenuOpen((prev) => !prev)
     }
-
-  return (
-    <div className="lg:hidden">
-        <Button onClick={toggleMobileMenu}>
-            {
-                isMobileMenuOpen ? (<ShieldCloseIcon />) : (<MenuIcon />)
-            }
-        </Button>
-    </div>
-  )
-}
-
-export default MobileMenu
+  
+    const {isDarkMode, toggleTheme}:any = useContext(ThemeContext)
+    
+    
+    return (
+        <Drawer  direction="right" open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
+        <DrawerTrigger asChild>
+          <Button variant="outline" className="lg:hidden"
+           onClick={toggleMobileMenu}>
+            <MenuIcon />
+          </Button>
+        </DrawerTrigger>
+  
+        <DrawerContent className="fixed right-0 top-0 h-full w-3/4 sm:w-1/2 bg-white dark:bg-gray-900 shadow-lg animate-in slide-in-from-right duration-500">
+          <nav className="flex flex-col space-y-10 px-7 py-10 ">
+            <DrawerClose asChild>
+               <span>Theme <Switch onClick={toggleTheme}/></span>
+            </DrawerClose>
+            <DrawerClose asChild>
+              <a href="/about" className="text-gray-700 dark:text-gray-300 hover:underline">News</a>
+            </DrawerClose>
+            <DrawerClose asChild>
+              <a href="/services" className="text-gray-700 dark:text-gray-300 hover:underline">Services</a>
+            </DrawerClose>
+            <DrawerClose asChild>
+              <a href="/blog" className="text-gray-700 dark:text-gray-300 hover:underline">Contact</a>
+            </DrawerClose>
+            <DrawerClose asChild>
+              <a href="/contact" className="text-gray-700 dark:text-gray-300 hover:underline">About</a>
+            </DrawerClose>
+          </nav>
+  
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button  className="w-full">Close</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    )
+  }
+  
+  export default MobileNav
+  
